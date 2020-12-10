aef561038b87bc682ab09ce2cdf8559670efb489
Stephen H. Gerstacker <stephen@gerstacker.us>
2020-12-10 08:08:19 -0500

    Day 10, Part 2 checkpoint

--- Day 10/main.swift
+++ Day 10/main.swift
@@ -34,3 +34,48 @@ sums[3] += 1
 print("Diffs: \(diffs)")
 print("Sums: \(sums)")
 print("Answer: \(sums[1] * sums[3])")
+
+// MARK: - Part 2
+
+let targetJoltage = joltages.last! + 3
+var matches: [[Int]] = []
+
+func visit(visited: [Int], remaining: [Int]) {
+    if (remaining.isEmpty) {
+        // print("* Match: \(visited)")
+        matches.append(visited)
+        return
+    }
+
+    let currentJoltage = visited.last ?? 0
+
+    // print("- (\(currentJoltage)): \(visited) - \(remaining)")
+
+    var invalidIndexes: [Int] = []
+    var nextIndexes: [Int] = []
+
+    for (idx, value) in remaining.enumerated() {
+        let diff = value - currentJoltage
+
+        if diff <= 0 {
+            invalidIndexes.append(idx)
+        } else if diff <= 3 {
+            nextIndexes.append(idx)
+        } else {
+            break
+        }
+    }
+
+    for index in nextIndexes {
+        var nextVisited = visited
+        var nextRemaining = remaining
+
+        nextVisited.append(nextRemaining.remove(at: index))
+        nextRemaining = nextRemaining.filter { $0 > nextVisited.last! }
+
+        visit(visited: nextVisited, remaining: nextRemaining)
+    }
+}
+
+visit(visited: [], remaining: joltages)
+print("Total Matches: \(matches.count)")
